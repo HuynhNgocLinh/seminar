1adc1b2a127de08aa635cfef121db88283ff4e9a
Linh Huynh <linhhuynh@fiot.vn>
2019-05-16 01:58:07 +0700

    update_parse_data_from_stm

---
+++ project_C#/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿using CircularBuffer;
+
+namespace Monitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.sendRecLabel = new System.Windows.Forms.Label();
+            this.viewBox = new System.Windows.Forms.TextBox();
+            this.comBox = new System.Windows.Forms.ComboBox();
+            this.baudRateBox = new System.Windows.Forms.ComboBox();
+            this.COMLabel = new System.Windows.Forms.Label();
+            this.baudRateLabel = new System.Windows.Forms.Label();
+            this.openBtn = new System.Windows.Forms.Button();
+            this.closeBtn = new System.Windows.Forms.Button();
+            this.serialPort = new System.IO.Ports.SerialPort(this.components);
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.viewStatusLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // sendRecLabel
+            // 
+            this.sendRecLabel.AutoSize = true;
+            this.sendRecLabel.Location = new System.Drawing.Point(16, 11);
+            this.sendRecLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.sendRecLabel.Name = "sendRecLabel";
+            this.sendRecLabel.Size = new System.Drawing.Size(136, 17);
+            this.sendRecLabel.TabIndex = 0;
+            this.sendRecLabel.Text = "Send/Received data";
+            // 
+            // viewBox
+            // 
+            this.viewBox.Location = new System.Drawing.Point(25, 34);
+            this.viewBox.Margin = new System.Windows.Forms.Padding(4);
+            this.viewBox.Multiline = true;
+            this.viewBox.Name = "viewBox";
+            this.viewBox.Size = new System.Drawing.Size(759, 360);
+            this.viewBox.TabIndex = 1;
+            // 
+            // comBox
+            // 
+            this.comBox.FormattingEnabled = true;
+            this.comBox.Location = new System.Drawing.Point(809, 57);
+            this.comBox.Margin = new System.Windows.Forms.Padding(4);
+            this.comBox.Name = "comBox";
+            this.comBox.Size = new System.Drawing.Size(160, 24);
+            this.comBox.TabIndex = 2;
+            // 
+            // baudRateBox
+            // 
+            this.baudRateBox.FormattingEnabled = true;
+            this.baudRateBox.Location = new System.Drawing.Point(809, 118);
+            this.baudRateBox.Margin = new System.Windows.Forms.Padding(4);
+            this.baudRateBox.Name = "baudRateBox";
+            this.baudRateBox.Size = new System.Drawing.Size(160, 24);
+            this.baudRateBox.TabIndex = 3;
+            // 
+            // COMLabel
+            // 
+            this.COMLabel.AutoSize = true;
+            this.COMLabel.Location = new System.Drawing.Point(805, 37);
+            this.COMLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.COMLabel.Name = "COMLabel";
+            this.COMLabel.Size = new System.Drawing.Size(39, 17);
+            this.COMLabel.TabIndex = 4;
+            this.COMLabel.Text = "COM";
+            // 
+            // baudRateLabel
+            // 
+            this.baudRateLabel.AutoSize = true;
+            this.baudRateLabel.Location = new System.Drawing.Point(805, 98);
+            this.baudRateLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.baudRateLabel.Name = "baudRateLabel";
+            this.baudRateLabel.Size = new System.Drawing.Size(70, 17);
+            this.baudRateLabel.TabIndex = 5;
+            this.baudRateLabel.Text = "Baud rate";
+            // 
+            // openBtn
+            // 
+            this.openBtn.Location = new System.Drawing.Point(837, 214);
+            this.openBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.openBtn.Name = "openBtn";
+            this.openBtn.Size = new System.Drawing.Size(100, 28);
+            this.openBtn.TabIndex = 6;
+            this.openBtn.Text = "Open";
+            this.openBtn.UseVisualStyleBackColor = true;
+            this.openBtn.Click += new System.EventHandler(this.OpenBtn_Click);
+            // 
+            // closeBtn
+            // 
+            this.closeBtn.Location = new System.Drawing.Point(837, 268);
+            this.closeBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.closeBtn.Name = "closeBtn";
+            this.closeBtn.Size = new System.Drawing.Size(100, 28);
+            this.closeBtn.TabIndex = 7;
+            this.closeBtn.Text = "Close";
+            this.closeBtn.UseVisualStyleBackColor = true;
+            this.closeBtn.Click += new System.EventHandler(this.CloseBtn_Click);
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 1;
+            this.timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(837, 326);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(48, 17);
+            this.statusLabel.TabIndex = 8;
+            this.statusLabel.Text = "Status";
+            // 
+            // viewStatusLabel
+            // 
+            this.viewStatusLabel.AutoSize = true;
+            this.viewStatusLabel.ForeColor = System.Drawing.Color.Red;
+            this.viewStatusLabel.Location = new System.Drawing.Point(839, 343);
+            this.viewStatusLabel.Name = "viewStatusLabel";
+            this.viewStatusLabel.Size = new System.Drawing.Size(94, 17);
+            this.viewStatusLabel.TabIndex = 9;
+            this.viewStatusLabel.Text = "Disconnected";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(995, 554);
+            this.Controls.Add(this.viewStatusLabel);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.closeBtn);
+            this.Controls.Add(this.openBtn);
+            this.Controls.Add(this.baudRateLabel);
+            this.Controls.Add(this.COMLabel);
+            this.Controls.Add(this.baudRateBox);
+            this.Controls.Add(this.comBox);
+            this.Controls.Add(this.viewBox);
+            this.Controls.Add(this.sendRecLabel);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label sendRecLabel;
+        private System.Windows.Forms.TextBox viewBox;
+        private System.Windows.Forms.ComboBox comBox;
+        private System.Windows.Forms.ComboBox baudRateBox;
+        private System.Windows.Forms.Label COMLabel;
+        private System.Windows.Forms.Label baudRateLabel;
+        private System.Windows.Forms.Button openBtn;
+        private System.Windows.Forms.Button closeBtn;
+        private System.IO.Ports.SerialPort serialPort;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.Label viewStatusLabel;
+    }
+}
+
---
+++ project_C#/Form1.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
+using CircularBuffer;
+
+
+namespace Monitor
+{
+  public enum USB_State_T
+  {
+    STATE_GET_HEADER,
+    STATE_GET_TYPE,
+    STATE_GET_CMD,
+    STATE_GET_PAYLOAD_SIZE,
+    STATE_GET_PAYLOAD_DATA,
+  }
+
+
+  public struct USB_Packet_T
+  {
+    public uint   Header;
+    public uint   Type;
+    public uint   Cmd;
+    public uint   Payload_Size;
+
+    public char[] Payload_Data;
+    
+    public USB_Packet_T(uint Header, uint Type, uint Cmd, uint Payload_Size, char[] Payload_Data)
+    {
+      this.Header       = Header;
+      this.Type         = Type;
+      this.Cmd          = Cmd;
+      this.Payload_Size = Payload_Size;
+      this.Payload_Data = Payload_Data;
+    }
+  } 
+
+  public partial class Form1 : Form
+  {
+    string inputData = string.Empty;
+    delegate void Set_Text_CB(string text);
+    
+    // Circular buffer
+    static CircularBuffer<char> cBuffer;
+
+    // Thread
+    Thread mThread;
+    
+    public Form1()
+    {
+      InitializeComponent();
+
+      serialPort.DataReceived += new SerialDataReceivedEventHandler(Data_Receive);
+      cBuffer = new CircularBuffer<char>(1000, false);
+
+      // Create thread
+      mThread = new Thread(USB_Process_Data);
+
+      // Start thread
+      mThread.Start();
+
+      string[] baudRate = { "1200", "2400", "4800", "9600", "19200", "38400", "57600", "115200" };
+
+      baudRateBox.Items.AddRange(baudRate);
+
+      //string[] COM = { "COM1", "COM2", "COM3", "COM4", "COM5" };
+
+      //comBox.Items.AddRange(COM);
+    }
+
+    public static void USB_Process_Data()
+    {
+      USB_State_T usbState = USB_State_T.STATE_GET_HEADER;
+      USB_Packet_T usbPacket = new USB_Packet_T(0, 0, 0, 0, new char[1000]);
+      for ( ; ; )
+      {
+        if (!cBuffer.IsEmpty)
+        {
+          switch (usbState)
+          {
+            case USB_State_T.STATE_GET_HEADER:
+            usbPacket.Header = Convert.ToByte(cBuffer.Get());
+
+              // Check header
+              if (usbPacket.Header == 0x55)
+              {
+                usbState = USB_State_T.STATE_GET_TYPE;
+              }
+              else
+              {
+                usbState = USB_State_T.STATE_GET_HEADER;
+              }
+
+              break;
+            case USB_State_T.STATE_GET_TYPE:
+              usbPacket.Type = Convert.ToByte(cBuffer.Get());
+
+              // Check type
+              if (usbPacket.Type <= 0x02)
+              {
+                usbState = USB_State_T.STATE_GET_CMD;
+              }
+              else
+              {
+                usbState = USB_State_T.STATE_GET_HEADER;
+              }
+
+              break;
+
+            case USB_State_T.STATE_GET_CMD:
+              usbPacket.Cmd = Convert.ToByte(cBuffer.Get());
+
+              // Check Cmd
+              if (usbPacket.Cmd <= 0x04)
+              {
+                usbState = usbState = USB_State_T.STATE_GET_PAYLOAD_SIZE;
+              }
+              else
+              {
+                usbState = USB_State_T.STATE_GET_HEADER;
+              }
+              
+            break;
+            case USB_State_T.STATE_GET_PAYLOAD_SIZE:
+              if (cBuffer.Size > 2)
+              {
+                //t_Data = Array.ConvertAll(cBuffer.Get(2), uint.Parse);
+                
+                //usbPacket.Payload_Size = (t_Data[0] << 8) | t_Data[1];
+                char[] t_Data = cBuffer.Get(2);
+
+                usbPacket.Payload_Size =  (uint)((t_Data[0] - '0') << 8) | t_Data[1];
+
+                if (usbPacket.Payload_Size <= 1000)
+                {
+                  usbState = USB_State_T.STATE_GET_PAYLOAD_DATA;
+                }
+                else 
+                {
+                  usbState = USB_State_T.STATE_GET_HEADER;
+                }
+              }
+
+              break;
+            case USB_State_T.STATE_GET_PAYLOAD_DATA:
+
+              usbPacket.Payload_Data = cBuffer.Get((int)usbPacket.Payload_Size);
+              if (cBuffer.Size >= usbPacket.Payload_Size)
+              {
+                // usbPacket.Payload_Data = Array.ConvertAll(cBuffer.Get((int)usbPacket.Payload_Size), char.Parse);
+                
+                // Clear packet data
+                usbPacket.Header = 0;
+                usbPacket.Type = 0;
+                usbPacket.Payload_Size = 0;
+                Array.Clear(usbPacket.Payload_Data, 0, usbPacket.Payload_Data.Length);
+
+                // Change to STATE_GET_HEADER
+                usbState = USB_State_T.STATE_GET_HEADER;
+
+              }
+                break;
+            default:
+                break;
+          }
+        }
+      }
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+        comBox.DataSource = SerialPort.GetPortNames();
+        baudRateBox.SelectedIndex = 7;
+    }
+
+    private void OpenBtn_Click(object sender, EventArgs e)
+    {
+        if (!serialPort.IsOpen)
+        {
+            serialPort.PortName = comBox.Text;
+            serialPort.BaudRate = Convert.ToInt32(baudRateBox.Text);
+
+            serialPort.Open();
+        }
+        else
+        {
+            serialPort.Close();
+        }
+    }
+
+    private void CloseBtn_Click(object sender, EventArgs e)
+    {
+        serialPort.Close();
+    }
+
+    private void Data_Receive(object obj, SerialDataReceivedEventArgs e)
+    {
+        inputData = serialPort.ReadExisting();
+        
+        // Write to circular buffer
+        cBuffer.Put(inputData.ToCharArray());
+
+        int tLen = cBuffer.Size;
+
+        //temp = serialPort.ReadByte();
+        if (inputData != String.Empty)
+        {
+            // 
+            // Test data
+            Set_Text(inputData);
+
+        }
+    }
+
+    private void Set_Text(string text)
+    {
+        if (this.viewBox.InvokeRequired)
+        {
+            Set_Text_CB d = new Set_Text_CB(Set_Text);
+
+            this.Invoke(d, new object[] { text });
+        }
+        else
+        {
+            this.viewBox.Text += text;
+        }
+    }
+
+    private void Timer_Tick(object sender, EventArgs e)
+    {
+        
+        //comBox.DataSource = SerialPort.GetPortNames();
+        if (!serialPort.IsOpen)
+        {
+            viewStatusLabel.Text = ("Disconnected");
+            viewStatusLabel.ForeColor = Color.Red;
+        }
+        else
+        {
+            viewStatusLabel.Text = ("Connected");
+            viewStatusLabel.ForeColor = Color.Green;
+        }
+    }
+
+    
+  }
+}
