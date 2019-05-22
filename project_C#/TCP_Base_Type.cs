ef768af509c8b7194945a4ee134942b25887f777
Linh Huynh <linhhuynh@fiot.vn>
2019-05-22 22:14:42 +0700

    Update class of TCP/IP command

---
+++ project_C#/TCP_Base_Type.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monitor
+{
+  public static class TCP_Base_Type
+  {
+    public enum TCP_IP_Status_T
+    {
+      TCP_IP_STATUS_OK,
+      TCP_IP_STATUS_ERR,
+    }
+
+    public struct TCP_IP_Info_T
+    {
+      public string ipAddr;
+      public int port;
+    }
+  }
+}
---
+++ project_C#/TCP_IP_Cmd.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Monitor
+{
+  class TCP_IP_Cmd
+  {
+    private TcpClient client;
+    private Stream stream;
+
+    public TCP_Base_Type.TCP_IP_Info_T checkStatusConnection()
+    {
+      TCP_Base_Type.TCP_IP_Info_T mInfo;
+      mInfo.ipAddr = ((IPEndPoint)client.Client.RemoteEndPoint).Address.ToString();
+      mInfo.port = ((IPEndPoint)client.Client.RemoteEndPoint).Port;
+
+      return mInfo;
+    }
+
+    public TCP_Base_Type.TCP_IP_Status_T establishConnetion(string ipAddr, int port)
+    {
+      try
+      {
+        client = new TcpClient();
+        client.Connect(ipAddr, port);
+        stream = client.GetStream();
+
+        return TCP_Base_Type.TCP_IP_Status_T.TCP_IP_STATUS_OK;
+      }
+      catch (Exception ex)
+      {
+        //Console.WriteLine("Error: " + ex);
+        return TCP_Base_Type.TCP_IP_Status_T.TCP_IP_STATUS_ERR;
+      }
+
+    }
+
+    public TCP_Base_Type.TCP_IP_Status_T sendData(string data)
+    {
+      try
+      {
+        var writer = new StreamWriter(stream);
+        writer.AutoFlush = true;
+        // Send
+        writer.WriteLine(data);
+        return TCP_Base_Type.TCP_IP_Status_T.TCP_IP_STATUS_OK;
+      }
+      catch (Exception ex)
+      {
+        return TCP_Base_Type.TCP_IP_Status_T.TCP_IP_STATUS_ERR;
+      }
+    }
+
+    public string receiveData()
+    {
+      var reader = new StreamReader(stream);
+
+      return reader.ReadLine();
+    }
+
+    public TCP_Base_Type.TCP_IP_Status_T closeConnection()
+    {
+      try
+      {
+        stream.Close();
+        client.Close();
+
+        return TCP_Base_Type.TCP_IP_Status_T.TCP_IP_STATUS_OK;
+      }
+      catch (Exception ex)
+      {
+        return TCP_Base_Type.TCP_IP_Status_T.TCP_IP_STATUS_ERR;
+      }
+    }
+  }
+}
